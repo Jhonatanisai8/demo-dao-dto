b8155d3fabaeafd9511e338d7cdf2476508516cb
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-08-20 21:38:43 -0500

    init

---
+++ src/main/java/com/isai/demo_dao_dto/DemoDaoDtoApplication.java
@@ -0,0 +1,13 @@
+package com.isai.demo_dao_dto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDaoDtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDaoDtoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isai/demo_dao_dto/DemoDaoDtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isai.demo_dao_dto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDaoDtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
