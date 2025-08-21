42976cbf7ca80457c29d884c61f9a76d0fb30880
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-08-20 22:12:15 -0500

    dto para user

---
+++ src/main/java/com/isai/demo_dao_dto/dto/UserDto.java
@@ -0,0 +1,21 @@
+package com.isai.demo_dao_dto.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private byte age;
+}
